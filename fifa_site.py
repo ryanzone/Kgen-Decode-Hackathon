40259c85f690f43856ea9f2a189524e8e3d4ecb2
Ryan John Mathew <75768806+ryanzone@users.noreply.github.com>
2025-04-16 21:29:24 +0530

    Update fifa_site.py

--- fifa_site.py
+++ fifa_site.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
